dfe35cbfdf3a375ba47b87909a9435f6837b95bc
Victor Gabriel <e008732@tst.jus.br>
2025-08-14 11:22:20 -0300

    Segundo commit

---
+++ src/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,11 @@
+import { Card } from './componentes/Card/Card';
+
+export const Cabecalho = () => {
+    return (
+        <Card>
+            <h1>Freelando</h1>
+        </Card>
+    );
+} 
+
+export default Cabecalho
