a38f596bcbd2dc5fecedd93863165d0e63f16936
Victor Gabriel <e008732@tst.jus.br>
2025-08-22 14:58:22 -0300

    Craindo rodape

--- src/componentes/CampoTexto/CampoText.jsx
+++ src/componentes/CampoTexto/CampoText.jsx
@@ -17,9 +17,11 @@ const InputEstilizado = styled.input`
     background: ${props => props.theme.cores.branco};
     border: 1px solid;
     border-color: ${props => props.theme.cores.neutras.a};
-    border-radius: ${props => props.theme.cores.espacamentos.s};;
+    border-radius: ${props => props.theme.espacamentos.s};;
     height: 40px;
-
+    font-weight: 400;
+    font-size:14px;
+    line-height: 17px;
 `
 
 export const CampoTexto = ({ titulo }) => {
